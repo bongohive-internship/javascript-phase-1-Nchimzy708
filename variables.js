17a91514642def7ad4c4e04fe4603166b1d59baf
nchimunya banda <nchimzy708@gmail.com>
2019-06-14 23:37:47 -0700

    Answered javascript phase 1..used extenal js to veiw the answers

--- variables.js
+++ variables.js
@@ -1,1 +1,11 @@
 // you solution should be here
+var birthYear = "2000";
+var futureYear= " 2027";
+var myAge = 2027 - 2000;
+console.log(myAge)
+
+var currentAge= 17;
+var maxAge = 77 ;
+var amountPerDay = " to eat 3 times a day";
+console.log("You will need"+" "+amountPerDay+" "+"to last you until the old ripe age of"+" "+ maxAge  )
+
